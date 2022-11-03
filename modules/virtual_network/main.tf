394f23f36ddcac2f38b4df7ee2403d6d79e2983c
Jeremy Cook <jeremy.cook@cloudacademy.com>
2022-11-04 12:00:56 +1300

    added HTTP allow to NSG

--- modules/virtual_network/main.tf
+++ modules/virtual_network/main.tf
@@ -31,12 +31,24 @@ resource "azurerm_network_security_group" "nsg1" {
 
   security_rule {
     name                       = "SSH"
-    priority                   = 1001
+    priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
